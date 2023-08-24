5c64add16bbb9986f4e01631dbc80719e5c8fb8d
Satyaswarupa parida <127578199+Satyaswarupa@users.noreply.github.com>
2023-08-24 16:00:26 +0530

    my landingPage

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import "./App.css"
+
+const App = () => {
+  return (
+    <div class="landing-page">
+    <header>
+      <div class="container">
+        <a href="#" class="logo">Satya<b>swarupa</b></a>
+        <ul class="links">
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Work</li>
+          <li>Info</li>
+          <li>Contact Me</li>
+        </ul>
+      </div>
+    </header>
+    <div class="content">
+      <div class="container">
+        <div class="info">
+          <h1>Looking For Inspiration</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus odit nihil ullam nesciunt quidem iste, Repellendus odit nihil</p>
+          <button>About me</button>
+        </div>
+        <div class="image">
+          <img src="https://i.postimg.cc/65QxYYzh/001234.png" />
+        </div>
+      </div>
+    </div>
+  </div>
+  )
+}
+
+export default App
